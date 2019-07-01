dc47741a4dad9e2d19b211a3b4fff4ed6d9bf86b
patradanai <bloody_jojo@hotmail.com>
2019-07-02 01:25:18 +0700

    ESP32

---
+++ ESP32_OPTO/ESP32_OPTO.ino
@@ -0,0 +1,26 @@
+#define Ch_1 26
+#define Ch_2 18
+#define Ch_3 19
+#define Ch_4 23
+#define Ch_5 5
+#define Ch_6 35
+#define Ch_7 33
+#define Ch_8 34
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+  pinMode(Ch_1, INPUT);
+  pinMode(Ch_2, INPUT);
+  pinMode(Ch_3, INPUT);
+  pinMode(Ch_4, INPUT);
+  pinMode(Ch_5, INPUT);
+  pinMode(Ch_6, INPUT);
+  pinMode(Ch_7, INPUT);
+  pinMode(Ch_8, INPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
